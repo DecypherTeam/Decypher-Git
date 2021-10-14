f7ac5538e2f9b5ae99bcf45c472ba2f3ba63211c
decypherGit <decyphergit@gmail.com>
2021-10-15 04:17:37 +0800

    One chicken patroled ayeeeee

--- Pengaga Ati/Assets/Scripts/PatrolDest.cs
+++ Pengaga Ati/Assets/Scripts/PatrolDest.cs
@@ -17,31 +17,31 @@ public class PatrolDest : MonoBehaviour
 
             if (pivotPoint == 4)
             {
-                this.gameObject.transform.position = new Vector3(14, -12, -30);
+                this.gameObject.transform.position = new Vector3(-5.09f, 1.42f, 10.52f);
                 pivotPoint = 5;
             }
 
             if (pivotPoint == 3)
             {
-                this.gameObject.transform.position = new Vector3(-8, -12, -33);
+                this.gameObject.transform.position = new Vector3(-13.26f, 1.6f, 12.63f);
                 pivotPoint = 4;
             }
 
             if (pivotPoint == 2)
             {
-                this.gameObject.transform.position = new Vector3(-24, -12, -33);
+                this.gameObject.transform.position = new Vector3(-15.51f, 1.53f, 4.53f);
                 pivotPoint = 3;
             }
 
             if (pivotPoint == 1)
             {
-                this.gameObject.transform.position = new Vector3(-17, -12, -20);
+                this.gameObject.transform.position = new Vector3(1, 1.51f, 12.73f);
                 pivotPoint = 2;
             }
 
             if (pivotPoint == 0)
             {
-                this.gameObject.transform.position = new Vector3(14, -12, -20);
+                this.gameObject.transform.position = new Vector3(10.29f, 1.58f, 15.18f);
                 pivotPoint = 1;
             }
         }
