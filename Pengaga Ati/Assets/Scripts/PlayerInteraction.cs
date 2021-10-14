64da59376ac0ace0ac6b84ee55d5612fcb9823c6
decypherGit <decyphergit@gmail.com>
2021-10-14 23:56:05 +0800

    Selection is working

--- Pengaga Ati/Assets/Scripts/PlayerInteraction.cs
+++ Pengaga Ati/Assets/Scripts/PlayerInteraction.cs
@@ -8,16 +8,12 @@ namespace Examples
     public class PlayerInteraction : MonoBehaviour
     {
         Player player;
-        //FarmingLand selectedLand = null;
 
-        // Start is called before the first frame update
         void Start()
         {
-            // Get access to our Player script component
-            player = transform.GetComponent<Player>();
+            player = transform.parent.GetComponent<Player>();
         }
 
-        // Update is called once per frame
         void Update()
         {
             RaycastHit hit;
@@ -27,42 +23,15 @@ namespace Examples
             }
         }
 
-        // Handles the interaction when player interact with farming land
         void OnInteractableHit(RaycastHit hit)
         {
             Collider other = hit.collider;
-            
-            //Check if player is going to interact with land
-            if(other.tag == "Land")
+            if (other.tag == "Land")
             {
-                //Debug.Log("select");
-                // Get the farming land script component
                 FarmingLand land = other.GetComponent<FarmingLand>();
                 land.Select(true);
-                //return;
             }
-
-            //Deselect the land if the player is not standing on any land at the moment
-            /*if(selectedLand != null)
-            {
-                selectedLand.Select(false);
-                selectedLand = null;
-            }*/
         }
-
-        //Handles the selection process
-        /*void SelectLand(FarmingLand farmingLand)
-        {
-            //Set the previously selected land to false (If any)
-            if(selectedLand != null)
-            {
-                selectedLand.Select(false);
-            }
-
-            //Set the new selected land to the land we're selecting now
-            selectedLand = farmingLand;
-            farmingLand.Select(true);
-        }*/
 
     }
 }
